a4c92cece73ee51b04b68ae7f614f42c5f3ae16d
AdminPin <1>
2023-01-05 15:48:51 +0300

    Лямбда тип функции

--- src/main/kotlin/Narrator.kt
+++ src/main/kotlin/Narrator.kt
@@ -3,11 +3,10 @@
 
 fun narrate( message: String) {
 
-    println({
-
+    val narrationModifier: () -> String = {
         val numExclamationPoints = 3
         message.uppercase() +"!".repeat(numExclamationPoints)
+    }
 
-    }()
-    )
+    println(narrationModifier())
 }
