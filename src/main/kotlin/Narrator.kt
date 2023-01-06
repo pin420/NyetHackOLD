a467cdf0231e004d15efe6fedc025555db9ee4da
AdminPin <1>
2023-01-06 18:34:41 +0300

    Эффективность лямбда-выражений

--- src/main/kotlin/Narrator.kt
+++ src/main/kotlin/Narrator.kt
@@ -5,15 +5,43 @@
 
 
 
+import kotlin.random.Random
+import kotlin.random.nextInt
 
-
-val narrationModifier= { message: String ->
-    val numExclamationPoints = 3
-    message.uppercase() +"!".repeat(numExclamationPoints)
-}
+var narrationModifier: (String) -> String = { it }
 
 fun narrate(
     message: String
 ) {
     println(narrationModifier(message))
+}
+
+fun changeNarratorMood() {
+    val mood: String
+    val modifier: (String) -> String
+    when (Random.nextInt(1..4)) {
+        1 -> {
+            mood = "loud"
+            modifier = { message ->
+                val numExclamationPoint = 3
+                message.uppercase() + "!".repeat(numExclamationPoint)
+            }
+        }
+        2 -> {
+            mood = "tired"
+            modifier = { message ->
+                message.lowercase().replace(" ", "... ")
+            }
+        }
+        3 -> {
+            mood = "unsure"
+            modifier = { message -> "$message?" }
+        }
+        else -> {
+            mood = "professional"
+            modifier = { message -> "$message." }
+        }
+    }
+    narrationModifier = modifier
+    narrate("The narrator begins to feel $mood")
 }
--- src/main/kotlin/NyetHack.kt
+++ src/main/kotlin/NyetHack.kt
@@ -4,4 +4,7 @@
 fun main() {
     narrate("A hero enters the town of Kronstadt. What is their name?")
     val heroName = readLine() ?: ""
+
+    changeNarratorMood()
+    narrate("$heroName heads to the town square")
 }
