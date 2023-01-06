9c8479a9f91c55db870a183057589b51c967f43c
AdminPin <1>
2023-01-06 22:28:51 +0300

    add Standart lib

--- src/main/kotlin/NyetHack.kt
+++ src/main/kotlin/NyetHack.kt
@@ -5,8 +5,21 @@ fun main() {
     narrate("A hero enters the town of Kronstadt. What is their name?"
     ) { message -> "\u001b[33;1m$message\u001b[0m" }
 
-    val heroName = readLine() ?: ""
+    val heroName = readLine()
+
+    require(!heroName.isNullOrEmpty()) {
+        "The hero must have a name"
+    }
 
     changeNarratorMood()
-    narrate("$heroName heads to the town square")
+    narrate("$heroName, ${createTitle(heroName)}, heads to the town square")
+}
+
+private fun createTitle(name: String): String {
+    return when {
+        name.all { letter -> letter.isDigit() } -> "The Identifiable"
+        name.none { letter -> letter.isLetter() } -> "The Witness Protection Member"
+        name.count { letter -> letter.lowercase() in "aeiou" } > 4 -> "The Master of Vowel"
+        else -> "The Renowned Hero"
+    }
 }
