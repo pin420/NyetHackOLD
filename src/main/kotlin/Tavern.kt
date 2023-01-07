f8d337ba469da9329960250f07f4695921567bb4
AdminPin <1>
2023-01-07 18:28:04 +0300

    Изменяемый список готов

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -13,6 +13,16 @@ private const val TAVERN_NAME = "$TAVERN_MASTER's Folly"
 fun visitTavern() {
     narrate("$heroName enters $TAVERN_NAME")
 
-    val patrons: List<String> = listOf("Eli", "Mordoc", "Sophie")
+    val patrons = mutableListOf("Eli", "Mordoc", "Sophie")
+
+    narrate("Eli leaves the tavern")
+    patrons.remove("Eli")
+    narrate("Alex enters the tavern")
+    patrons.add("Alex")
+    println(patrons)
+
+    narrate("Alex (VIP) enters the tavern")
+    patrons.add(0, "Alex")
+    patrons[0] = "Alexis"
     println(patrons)
 }
