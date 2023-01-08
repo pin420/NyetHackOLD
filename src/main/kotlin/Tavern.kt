bdefadaebf2c93cca35461fbfc9566177e44df8c
AdminPin <1>
2023-01-08 04:16:45 +0300

    Деструкторизация из списка в список

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -5,20 +5,22 @@ private const val TAVERN_NAME = "$TAVERN_MASTER's Folly"
 private val menuData = File("data/tavern-menu-data.txt")
     .readText()
     .split("\n")
+private val menuItem = List(menuData.size) { index ->
+    val (_, name, _) = menuData[index].split(",")
+    name
+}
 
 
 fun visitTavern() {
     narrate("$heroName enters $TAVERN_NAME")
+    narrate("There are several items for sale:")
+    println(menuItem)
 
     val patrons = mutableListOf("Eli", "Mordoc", "Sophie")
 
     patrons.forEachIndexed { index, patron ->
         println("Good evening, $patron - you're #${index + 1} in line")
-        placeOrder(patron, "Dragon's Breath")
-    }
-
-    menuData.forEachIndexed { index, data ->
-        println("$index : $data")
+        placeOrder(patron, menuItem.random())
     }
 }
 
