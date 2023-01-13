5dfafefba0a577d1fa6b4f0c489ae0a7aa73ecff
AdminPin <1>
2023-01-13 21:41:16 +0300

    get и set методы свойств

--- src/main/kotlin/Player.kt
+++ src/main/kotlin/Player.kt
@@ -3,7 +3,11 @@
 
 class Player {
 
-    val name = "Madrigal"
+    var name = "madrigal"
+        get() = field.replaceFirstChar { char -> char.uppercase() }
+        set(value) {
+            field = value.trim()
+        }
 
     fun castFireball(numFireballs: Int = 2) {
         narrate("A glass of Fireballs springs into existence (x$numFireballs)")
