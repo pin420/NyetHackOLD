0f109ebdadf9adc56fa0d3112812b4c393723b05
AdminPin <1>
2023-01-19 20:03:05 +0300

    Классы-перечисления enum

--- src/main/kotlin/com/bignerdranch/nyethack/Navigation.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/Navigation.kt
@@ -1,3 +1,18 @@
 package com.bignerdranch.nyethack
 
 data class Coordinate(val x: Int, val y: Int)
+
+enum class Direction(
+    private val directionCoordinate: Coordinate
+) {
+    North(Coordinate(0, -1)),
+    East(Coordinate(1, 0)),
+    South(Coordinate(0, 1)),
+    West(Coordinate(-1, 0));
+
+    fun updateCoordinate(coordinate: Coordinate) =
+        Coordinate(
+            x = coordinate.x + directionCoordinate.x,
+            y = coordinate.y + directionCoordinate.y
+        )
+}
