0412df1ae5c91ce792c5b60c87d4469bf86b397c
AdminPin <1>
2023-01-19 22:05:42 +0300

    Создание карты мира NyetHack

--- src/main/kotlin/com/bignerdranch/nyethack/Navigation.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/Navigation.kt
@@ -1,6 +1,8 @@
 package com.bignerdranch.nyethack
 
-data class Coordinate(val x: Int, val y: Int)
+data class Coordinate(val x: Int, val y: Int) {
+    operator fun plus(other: Coordinate) = Coordinate(x + other.x, y + other.y)
+}
 
 enum class Direction(
     private val directionCoordinate: Coordinate
@@ -10,9 +12,5 @@ enum class Direction(
     South(Coordinate(0, 1)),
     West(Coordinate(-1, 0));
 
-    fun updateCoordinate(coordinate: Coordinate) =
-        Coordinate(
-            x = coordinate.x + directionCoordinate.x,
-            y = coordinate.y + directionCoordinate.y
-        )
+    fun updateCoordinate(coordinate: Coordinate) = coordinate + directionCoordinate
 }
--- src/main/kotlin/com/bignerdranch/nyethack/NyetHack.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/NyetHack.kt
@@ -13,7 +13,6 @@ fun main() {
 //    com.bignerdranch.nyethack.changeNarratorMood()
 
 
-
     Game.play()
 }
 
@@ -34,7 +33,13 @@ private fun promptHeroName(): String {
 
 
 object Game {
-    private var currentRoom: Room = TownSquare()
+    private val worldMap = listOf(
+        listOf(TownSquare(), Tavern(), Room("Back Room")),
+        listOf(Room("A Long Corridor"), Room("A Generic Room")),
+        listOf(Room("The Dungeon"))
+    )
+
+    private var currentRoom: Room = worldMap[0][0]
     private var currentPosition = Coordinate(0, 0)
 
     init {
@@ -54,12 +59,34 @@ object Game {
         }
     }
 
+    fun move(direction: Direction) {
+        val newPosition = direction.updateCoordinate(currentPosition)
+        val newRoom = worldMap.getOrNull(newPosition.y)?.getOrNull(newPosition.x)
+
+        if (newRoom != null) {
+            narrate("The hero moves ${direction.name}")
+            currentPosition = newPosition
+            currentRoom = newRoom
+        } else {
+            narrate("You cannot move ${direction.name}")
+        }
+    }
+
     private class GameInput(arg: String?) {
         private val input = arg ?: ""
         val command = input.split(" ")[0]
         val argument = input.split(" ").getOrElse(1) { "" }
 
         fun processCommand() = when (command.lowercase()) {
+            "move" -> {
+                val direction = Direction.values()
+                    .firstOrNull { it.name.equals(argument, ignoreCase = true) }
+                if (direction != null){
+                    move(direction)
+                } else {
+                    narrate("I don't know what direction that is")
+                }
+            }
             else -> narrate("I'm not sure what you're trying to do")
         }
     }
