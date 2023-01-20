1411417134de66ff2759bb991593e094c7b3634c
AdminPin <1>
2023-01-20 06:09:46 +0300

    Реализация интерфейса

--- src/main/kotlin/com/bignerdranch/nyethack/Player.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/Player.kt
@@ -3,15 +3,16 @@ package com.bignerdranch.nyethack
 class Player(
     initialName: String,
     val hometown: String = "Neversummer",
-    var healthPoints: Int,
+    override var healthPoints: Int,
     val isImmortal: Boolean
-) {
+) : Fightable {
 
-    var name = initialName
+    override var name = initialName
         get() = field.replaceFirstChar { char -> char.uppercase() }
         private set(value) {
             field = value.trim()
         }
+
 
     val title: String
         get() = when {
@@ -32,6 +33,10 @@ class Player(
             "best the world-eater"
         ).random()
     }
+
+    override val diceCount = 3
+
+    override val diceSides = 4
 
     init {
         require(healthPoints > 0 ) {"healthPoints must be greater than zero"}
@@ -60,5 +65,15 @@ class Player(
     fun prophesize() {
         narrate("$name thinks about their future")
         narrate("A fortune teller told Madrigal, \"$prophecy\"")
+    }
+
+    override fun takeDamage(damage: Int) {
+        if (!isImmortal) {
+            healthPoints -= damage
+        }
+    }
+
+    override fun attack(opponent: Fightable) {
+        TODO("Not yet implemented")
     }
 }
