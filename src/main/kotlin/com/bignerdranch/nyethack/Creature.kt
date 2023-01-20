49b4bc0f3f97a1e1d2e26011fad150be418f7b1f
AdminPin <1>
2023-01-20 07:26:54 +0300

    Создание подкласса из абстрактного класса и реализация его свойств

--- src/main/kotlin/com/bignerdranch/nyethack/Creature.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/Creature.kt
@@ -32,3 +32,12 @@ abstract class Monster(
         healthPoints -= damage
     }
 }
+
+class Goblin(
+    name: String = "Goblin",
+    description: String = "A nasty-looking goblin",
+    healthPoints: Int = 30
+) : Monster(name, description, healthPoints) {
+    override val diceCount: Int = 2
+    override val diceSides: Int = 8
+}
